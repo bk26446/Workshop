da380fce435e1f081525e52270f90ec2d7224a5b
bk26446 <bk26446@essex.ac.uk>
2026-02-28 16:58:47 +0000

    Update memory.py

--- Virtual_assistant/chatbot/memory.py
+++ Virtual_assistant/chatbot/memory.py
@@ -1,19 +1,50 @@
+import json
+import os
+
+
+
 class ConversationMemory:
     
     """
   Stores and manages conversation context.
   """
     
-    def __init__(self):
-          # Dictionary to store remembered facts
-          self.memory = {}
+    def __init__(self, file_path="memory.json"):
+        # Dictionary to store remembered facts
+        self.file_path = file_path
+        self.memory = {}
+        
+        self._load_memory()
+        
+    def _load_memory(self):
+        
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as file:
+                self.memory = json.load(file)
+                
+    def _save_memory(self):
+        
+        "Saves a memory to disk."
+        
+        with open (self.file_path, "w") as file:
+            json.dump(self.memory, file, indent=4)
+            
+            # KEY LINE: Memory persistence
+            
+        
+ 
+        
+       
           # ⚠️ KEY LINE: Centralized state storage
-    def remember(seld, key:str, value: str):
+          
+    def remember(self, key:str, value: str):
         """
         Saves information to memory.
         """
 
-        seld.memory[key] = value
+        self.memory[key] = value
+        self._save_memory()
+        
         
     def recall(self, key: str):
         """
@@ -30,7 +61,6 @@ class ConversationMemory:
         return key in self.memory
 
         
-
     
          
   
@@ -54,3 +84,4 @@ class ConversationMemory:
           
 
        
+
